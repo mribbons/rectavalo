68f5ef51728df5721776406011b4b05cbc67b86d
Mike Ribbons <mribbons@gmail.com>
2023-06-21 21:26:25 +1000

    win32: update c++ standard, fix corresponding wstring errors

--- cxx/libload.cpp
+++ cxx/libload.cpp
@@ -5,6 +5,9 @@
 #include <rectavalo_io.hpp>
 #include <codecvt>
 #include <locale>
+#ifdef _WIN32
+#include <Windows.h>
+#endif
 
 std::string value = "Hello from C++!";
 
@@ -39,11 +42,38 @@ Json::Value parseJson(const std::string& json) {
   return root;
 }
 
+#ifdef _WIN32
+// Convert a wide Unicode string to an UTF8 string
+std::string utf8_encode(const std::wstring& wstr)
+{
+  if (wstr.empty()) return std::string();
+  int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+  std::string strTo(size_needed, 0);
+  WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+  return strTo;
+}
+
+// Convert an UTF8 string to a wide Unicode String
+std::wstring utf8_decode(const std::string& str)
+{
+  if (str.empty()) return std::wstring();
+  int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+  std::wstring wstrTo(size_needed, 0);
+  MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+  return wstrTo;
+}
+#endif
+
 Json::Value parseJson(const std::wstring& wide) {
-  using convert_type = std::codecvt_utf8<wchar_t>;
+  std::string narrow;
+#ifndef _WIN32
+    using convert_type = std::codecvt_utf8<wchar_t>;
   std::wstring_convert<convert_type, wchar_t> converter;
   //use converter (.to_bytes: wstr->str, .from_bytes: str->wstr)
-  std::string narrow = converter.to_bytes(wide);
+  narrow = converter.to_bytes(wide);
+#else
+    narrow = utf8_encode(wide);
+  #endif
   return parseJson(narrow);
 }
 
